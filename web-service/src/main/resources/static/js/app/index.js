51fbca052d16f550ba42b784ecf38438f06385a9
jerryJK <vonjobi2@gmail.com>
2020-07-28 02:01:26 +0900

    Add 'GET /' findAllDesc

--- web-service/src/main/resources/static/js/app/index.js
+++ web-service/src/main/resources/static/js/app/index.js
@@ -3,7 +3,7 @@ var main = {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
-        })
+        });
     },
     save : function () {
         var data = {
@@ -17,7 +17,7 @@ var main = {
             url: '/api/v1/posts',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
-            data: JSon.stringify(data)
+            data: JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
@@ -27,4 +27,4 @@ var main = {
     }
 };
 
-index.init();
+main.init();
